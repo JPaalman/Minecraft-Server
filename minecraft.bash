dea510ee5e086b379e1edac26b3b572669e3356c
Jaïr Paalman <jair.paalman@outlook.com>
2020-11-22 04:17:18 +0100

    Bukkit, Spigot and Paper support

--- minecraft.bash
+++ minecraft.bash
@@ -3,87 +3,104 @@
 # usage: [path_to_script]/minecraft.bash [functions_to_run]
 # example: /srv/minecraft/minecraft.bash start console
 
-# ---------------- settings ----------------
+# -------------------------------- settings --------------------------------
 
-# paths (relative to script directory)
-WORLDNAME="matigcraft" # should be the same as level-name in server.properties
+# paths
 BACKUP_DIRECTORY="backup"
-
-# log all screen content to $WORLDNAME.log
-LOGGING=true
-
-# JVM arguments to use when starting the server
-JVM_ARGUMENTS="-Xms4G -Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC"
+SERVER_NAME="paper-288" # the filename of your server.jar
+WORLD_NAME="matigcraft" # level-name in server.properties
 
 # messages
-STOP_KICK_MESSAGE="The server is shutting down for maintenance"
 BACKUP_KICK_MESSAGE="The server is making a backup, and will be back online in a few minutes"
-STOP_WARNING_MESSAGE="The server will go offline for maintenance in X minute(s)"
 BACKUP_WARNING_MESSAGE="The server will go offline to make a backup in X minute(s)"
+STOP_KICK_MESSAGE="The server is shutting down for maintenance"
+STOP_WARNING_MESSAGE="The server will go offline for maintenance in X minute(s)"
 
-# amount of backups to keep when cleaning
-BACKUP_AMOUNT=14
+# enable if you are using Bukkit, Spigot or Paper
+ALTERNATIVE_SERVER=true
 
 # clean automatically after a backup
-AUTOCLEAN=true
+AUTO_CLEAN=true
 
-# ---------------- functions ----------------
+# amount of backups to keep when cleaning
+BACKUP_AMOUNT=28
+
+# JVM arguments to use when starting the server
+JVM_ARGUMENTS="-Xms6G -Xmx6G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
+
+# log all screen content to $WORLD_NAME.log
+LOGGING=true
+
+# -------------------------------- functions --------------------------------
 
 # check if the server is running
 function status {
-  screen -ls | grep -q -w "$WORLDNAME" \
-  && echo $WORLDNAME is running && return 0 \
-  || echo $WORLDNAME is not running && return 1
+  screen -ls | grep -q -w "$WORLD_NAME" \
+  && echo $WORLD_NAME is running && return 0 \
+  || echo $WORLD_NAME is not running && return 1
 }
 
 # if not running, start the server in the background
 function start {
   status > /dev/null || {
-    $LOGGING && printf "\n\n" >> "$WORLDNAME".log && date >> "$WORLDNAME".log
-    screen $($LOGGING && echo -L -Logfile "$WORLDNAME".log) -DmS "$WORLDNAME" java $JVM_ARGUMENTS -jar server.jar nogui &
-  }
-}
-
-# if running, stop the server and wait
-# call with number to set as warning delay
-# call with additional text to use as kick message, defaults to STOP_KICK_MESSAGE
-# call with additional text to use as warning message, defaults to STOP_WARNING_MESSAGE
-function stop {
-  status > /dev/null && {
-    [ -z "$1" ] || {
-      screen -S "$WORLDNAME" -X stuff "say ${{3:-$STOP_WARNING_MESSAGE}\n//X/$1}\n"
-      sleep $(($1 * 60))
+    $LOGGING && {
+      printf "\n\n" >> "$WORLD_NAME".log && date >> "$WORLD_NAME".log
+      screen -L -Logfile "$WORLD_NAME".log -DmS "$WORLD_NAME" java $JVM_ARGUMENTS -jar "$SERVER_NAME" nogui &
+      return
     }
-    screen -S "$WORLDNAME" -X stuff "kick @a ${2:-$STOP_KICK_MESSAGE}\n""stop\n"
-    while status > /dev/null; do sleep 1; done
+    screen -DmS "$WORLD_NAME" java $JVM_ARGUMENTS -jar "$SERVER_NAME" nogui &
   }
 }
 
 # open the server console (CTRL + A, D to close)
 function console {
-  screen -d -r "$WORLDNAME"
+  screen -d -r "$WORLD_NAME"
+}
+
+# if running, stop the server and wait
+# call with number to set as warning delay
+# call with additional text to use as warning message, defaults to STOP_WARNING_MESSAGE
+# call with additional text to use as kick message, defaults to STOP_KICK_MESSAGE
+function stop {
+  status > /dev/null && {
+    [ -z "$1" ] || {
+      screen -S "$WORLD_NAME" -X stuff "say ${{2:-$STOP_WARNING_MESSAGE}\n//X/$1}\n"
+      sleep $(($1 * 60))
+    }
+    screen -S "$WORLD_NAME" -X stuff "kick @a ${3:-$STOP_KICK_MESSAGE}\n""stop\n"
+    while status > /dev/null; do sleep 1; done
+  }
 }
 
 # make a backup of the world
 # call with number to set as warning delay
-# call with additional text to use as kick message, defaults to BACKUP_KICK_MESSAGE
 # call with additional text to use as warning message, defaults to BACKUP_WARNING_MESSAGE
+# call with additional text to use as kick message, defaults to BACKUP_KICK_MESSAGE
 function backup {
+  $ALTERNATIVE_SERVER && convert
   mkdir -p "$BACKUP_DIRECTORY"
-  stop $1 "${2:-$BACKUP_KICK_MESSAGE}" "${3:-$BACKUP_WARNING_MESSAGE}"
-  zip -9 -r "$BACKUP_DIRECTORY"/"$WORLDNAME"_"$(date +%Y_%m_%d_%H%M)".zip "$WORLDNAME"
-  $AUTOCLEAN && clean
+  stop "$1" "${2:-$BACKUP_WARNING_MESSAGE}" "${3:-$BACKUP_KICK_MESSAGE}"
+  zip -9 -r "$BACKUP_DIRECTORY"/"$WORLD_NAME"_"$(date +%Y_%m_%d_%H%M)".zip "$WORLD_NAME"
+  $AUTO_CLEAN && clean
+}
+
+# convert Bukkit, Spigot and Paper worlds back to vanilla format
+function convert {
+  mv "$WORLD_NAME"_nether/DIM-1 "$WORLD_NAME"/DIM-1
+  mv "$WORLD_NAME"_the_end/DIM1 "$WORLD_NAME"/DIM1
+  rm -rf "$WORLD_NAME"_nether
+  rm -rf "$WORLD_NAME"_the_end
 }
 
 # clean old backups
 function clean {
-  find "$BACKUP_DIRECTORY"/"$WORLDNAME"* |
+  find "$BACKUP_DIRECTORY"/"$WORLD_NAME"* |
   sort -r |
   cut -d $'\n' -f $((BACKUP_AMOUNT + 1))- |
   xargs rm
 }
 
-# ---------------- main ----------------
+# -------------------------------- main --------------------------------
 
 # make sure to run as user 'minecraft'
 [ "$USER" == minecraft ] || { sudo -u minecraft "$0" "$@"; exit; }
