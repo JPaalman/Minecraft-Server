e8f42190bbf6b7184e420e3a08d1a4a2f71919b8
Jaïr Paalman <35742282+JPaalman@users.noreply.github.com>
2023-04-18 04:59:54 +0200

    Added "message" function

--- minecraft.bash
+++ minecraft.bash
@@ -18,11 +18,9 @@ JVM_ARGUMENTS="-Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCP
 # log all screen content to $WORLD_NAME.log
 LOGGING=true
 
-# messages
-BACKUP_KICK_MESSAGE="The server is restarting to make a backup, and will be back online in a few minutes"
-BACKUP_WARNING_MESSAGE="The server will restart to make a backup in X minute(s)"
-STOP_KICK_MESSAGE="The server is shutting down for maintenance"
-STOP_WARNING_MESSAGE="The server will shut down for maintenance in X minute(s)"
+# kick messages
+BACKUP_MESSAGE="The server is restarting to make a backup, and will be back online in a few minutes"
+STOP_MESSAGE="The server is shutting down for maintenance"
 
 # has to be the same as level-name in server.properties
 WORLD_NAME="matigcraft"
@@ -36,10 +34,34 @@ function status {
     echo $WORLD_NAME is not running && return 1
 }
 
+# restore converted vanilla worlds to Bukkit, Spigot, Paper and similar server format
+function convert_bukkit {
+  [ -d "$WORLD_NAME"/"$WORLD_NAME"_nether ] && {
+    mv "$WORLD_NAME"/"$WORLD_NAME"_nether ./
+    mv "$WORLD_NAME"/DIM-1 "$WORLD_NAME"_nether
+  }
+  [ -d "$WORLD_NAME"/"$WORLD_NAME"_the_end ] && {
+    mv "$WORLD_NAME"/"$WORLD_NAME"_the_end ./
+    mv "$WORLD_NAME"/DIM1 "$WORLD_NAME"_the_end
+  }
+}
+
+# convert Bukkit, Spigot, Paper and similar server worlds back to vanilla format
+function convert_vanilla {
+  [ -d "$WORLD_NAME"_nether ] && {
+    mv "$WORLD_NAME"_nether/DIM-1 "$WORLD_NAME"
+    mv "$WORLD_NAME"_nether "$WORLD_NAME"
+  }
+  [ -d "$WORLD_NAME"_the_end ] && {
+    mv "$WORLD_NAME"_the_end/DIM1 "$WORLD_NAME"
+    mv "$WORLD_NAME"_the_end "$WORLD_NAME"
+  }
+}
+
 # if not running, start the server in the background
 function start {
   status >/dev/null || {
-    convert_vanilla
+    convert_bukkit
     $LOGGING && {
       printf "\n\n" >>"$WORLD_NAME".log && date >>"$WORLD_NAME".log
       screen -L -Logfile "$WORLD_NAME".log -DmS "$WORLD_NAME" java $JVM_ARGUMENTS -jar server.jar nogui &
@@ -54,55 +76,28 @@ function console {
   screen -d -r "$WORLD_NAME"
 }
 
+# if running, send a chat message
+# call with message
+function message {
+   status >/dev/null && screen -S "$WORLD_NAME" -X stuff "say $1"
+}
+
 # if running, stop the server and wait
-# call with number to set as warning delay
-# call with additional text to use as warning message, defaults to STOP_WARNING_MESSAGE
-# call with additional text to use as kick message, defaults to STOP_KICK_MESSAGE
+# call with additional text to use as kick message, defaults to STOP_MESSAGE
 function stop {
   status >/dev/null && {
-    [ -z "$1" ] || {
-      T="${2:-$STOP_WARNING_MESSAGE}"
-      screen -S "$WORLD_NAME" -X stuff "say ${T//X/$1}\n"
-      sleep $(($1 * 60))
-    }
-    screen -S "$WORLD_NAME" -X stuff "kick @a ${3:-$STOP_KICK_MESSAGE}\n""stop\n"
+    screen -S "$WORLD_NAME" -X stuff "kick @a ${1:-$STOP_MESSAGE}\n""stop\n"
     while status >/dev/null; do sleep 1; done
   }
 }
 
 # make a backup of the world
-# call with number to set as warning delay
-# call with additional text to use as warning message, defaults to BACKUP_WARNING_MESSAGE
-# call with additional text to use as kick message, defaults to BACKUP_KICK_MESSAGE
+# call with additional text to use as kick message, defaults to BACKUP_MESSAGE
 function backup {
-  stop "$1" "${2:-$BACKUP_WARNING_MESSAGE}" "${3:-$BACKUP_KICK_MESSAGE}"
-  convert_alternative
+  stop "${1:-$BACKUP_KICK_MESSAGE}"
+  convert_vanilla
   mkdir -p "$BACKUP_DIRECTORY"
   zip -9 -r "$BACKUP_DIRECTORY"/"$WORLD_NAME"_"$(date +%Y_%m_%d_%H%M)".zip "$WORLD_NAME"
-}
-
-# convert Bukkit, Spigot, Paper and similar server worlds back to vanilla format
-function convert_alternative {
-  [ -d "$WORLD_NAME"_nether ] && {
-    mv "$WORLD_NAME"_nether/DIM-1 "$WORLD_NAME"
-    mv "$WORLD_NAME"_nether "$WORLD_NAME"
-  }
-  [ -d "$WORLD_NAME"_the_end ] && {
-    mv "$WORLD_NAME"_the_end/DIM1 "$WORLD_NAME"
-    mv "$WORLD_NAME"_the_end "$WORLD_NAME"
-  }
-}
-
-# restore converted vanilla worlds to Bukkit, Spigot, Paper and similar server format
-function convert_vanilla {
-  [ -d "$WORLD_NAME"/"$WORLD_NAME"_nether ] && {
-    mv "$WORLD_NAME"/"$WORLD_NAME"_nether ./
-    mv "$WORLD_NAME"/DIM-1 "$WORLD_NAME"_nether
-  }
-  [ -d "$WORLD_NAME"/"$WORLD_NAME"_the_end ] && {
-    mv "$WORLD_NAME"/"$WORLD_NAME"_the_end ./
-    mv "$WORLD_NAME"/DIM1 "$WORLD_NAME"_the_end
-  }
 }
 
 # clean old backups
